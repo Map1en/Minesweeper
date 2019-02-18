497e093e6bdeca603bf2587a62cf8fe0105af627
Map1en_ <maplenagisa@gmail.com>
2019-02-18 22:43:26 +0800

    基本功能完成

--- mine.js
+++ mine.js
@@ -1,217 +1,311 @@
-document.oncontextmenu = function (event) {
-    event.preventDefault();
-};
+//Data
 let mineNum = 10;
-let sLength = 9;
+let flaggedMine = 0;
+let xLength = 9;
+let yLength = 9;
 let step = 0;
 let openedBlock = 0;
 let time = 0;
+let timer;
+let isOver = false;
 
-class Block {
+let bArea = document.getElementById("block-area");
+
+//DOM
+document.oncontextmenu = function (event) {
+    event.preventDefault();
+};
+document.getElementById("easy").addEventListener("click", () => selectLevel("easy"));
+document.getElementById("normal").addEventListener("click", () => selectLevel("normal"));
+document.getElementById("hard").addEventListener("click", () => selectLevel("hard"));
+document.getElementById("newBtn").addEventListener("click", () => newGame(xLength, yLength, mineNum));
+document.getElementById("mine").innerHTML = mineNum;
+document.getElementById("step").innerHTML = step;
+document.getElementById("timer").innerHTML = time;
+
+//Class
+class Attr {
     constructor() {
         this.opened = false;
         this.flagged = false;
-        this.ismine = false;
+        this.isMine = false;
     }
 }
 
-class Area {
+class Block {
     constructor() {
         this.array = [];
+        this.notMine;
     }
-    setBlock(x, y) {
-        for (let i = 0; i < sLength; i++) {
+    setMine(x, y) {
+        for (let i = 0; i < xLength; i++) {
             this.array[i] = [];
-            for (let j = 0; j < sLength; j++) {
-                this.array[i][j] = new Block();
+            for (let j = 0; j < yLength; j++) {
+                this.array[i][j] = new Attr();
             }
         }
         for (let k = 0; k < mineNum; k++) {
-            const r1 = ~~(Math.random() * sLength);
-            const r2 = ~~(Math.random() * sLength);
-            if (r1 == x && r2 == y) {      //第一次点击不会踩雷
+            const R1 = ~~(Math.random() * xLength);
+            const R2 = ~~(Math.random() * yLength);
+            if (R1 == x && R2 == y) {      //第一次点击不会踩雷
                 k--;
-            } else if (this.array[r1][r2].ismine) {
+            } else if (this.array[R1][R2].isMine) {
                 k--;
             } else {
-                this.array[r1][r2].ismine = true;
+                this.array[R1][R2].isMine = true;
             }
         }
     }
-    setFlag(x, y) {
-        this.array[x][y].flagged = true;
-        mineNum--;
-    }
-    setOpened(x, y) {
-        this.array[x][y].opened = true;
-    }
-}
-
-let a1 = new Area();
-let bArea = document.getElementById("block-area");
-
-
-function newGame(x, y) {
-    time = 0;
-    timer = setInterval(() => {           //issue：多次点击好像会创建很多timer，加速计时
-        time++;
-        document.getElementById("timer").innerHTML = time;
-    }, 1000);
-    step = 0;
-    openedBlock = 0;
-
-    for (let i = 0; i < y; i++) {
-        let newDivy = document.createElement("div");
-        newDivy.className = "row";
-        for (let j = 0; j < x; j++) {
-            let newDivx = document.createElement("div");
-            newDivx.className = "unopened";
-            newDivx.setAttribute("x", j);
-            newDivx.setAttribute("y", i);
-            newDivx.addEventListener(
-                "click", (event) =>open(
-                    event.srcElement.attributes.x.value, event.srcElement.attributes.y.value
-                )
-            );
-            newDivx.addEventListener(
-                "auxclick", (event) =>flag(
-                    event.srcElement.attributes.x.value, event.srcElement.attributes.y.value
-                )
-            );
-            newDivy.appendChild(newDivx);
+    removeMinefield() {
+        while (bArea.firstChild) {
+            bArea.removeChild(bArea.firstChild);
         }
-        bArea.appendChild(newDivy);
+    }
+    setMinefield(x, y) {
+        this.notMine = xLength * yLength - mineNum;
+        for (let i = 0; i < y; i++) {
+            let newDivy = document.createElement("div");
+            newDivy.className = "row";
+            for (let j = 0; j < x; j++) {
+                let newDivx = document.createElement("div");
+                newDivx.className = "unopened";
+                newDivx.setAttribute("x", j);
+                newDivx.setAttribute("y", i);
+                newDivx.addEventListener(
+                    "click", (event) => this.perOpen(
+                        event.srcElement.attributes.x.value, event.srcElement.attributes.y.value
+                    )
+                );
+                newDivx.addEventListener(
+                    "auxclick", (event) => this.setFlag(
+                        event.srcElement.attributes.x.value, event.srcElement.attributes.y.value
+                    )
+                );
+                newDivy.appendChild(newDivx);
+            }
+            bArea.appendChild(newDivy);
+        }
+    }
+    setFlag(x, y) {
+        if (this.array[0] == undefined) {
+            this.setMine(x, y);
+            Timer();
+        }
+        if (!this.array[x][y].opened) {
+            if (!this.array[x][y].flagged) {
+                this.array[x][y].flagged = true;
+                bArea.children[y].children[x].classList.add("flagged");
+                if (flaggedMine < mineNum) {
+                    flaggedMine++;
+                }
+                document.getElementById("mine").innerHTML = mineNum - flaggedMine;
+            } else {
+                this.array[x][y].flagged = false;
+                bArea.children[y].children[x].classList.remove("flagged");
+            }
+        }
+    }
+    perOpen(x, y) {
+        if (this.array[0] == undefined) {
+            this.setMine(x, y);
+            Timer();
+        }
+        if (!isOver && !this.array[x][y].opened) {
+            step++;
+            document.getElementById("step").innerHTML = step;
+            this.openBlock(x, y);
+        }
+    }
+    openBlock(x, y) {
+        if (!this.array[x][y].isMine) {
+            if (!this.array[x][y].flagged) {
+                this.array[x][y].opened = true;
+                bArea.children[y].children[x].setAttribute("class", "opened");
+                openedBlock++;
+                if (openedBlock == this.notMine) {
+                    for (let i = 0; i < xLength; i++) {
+                        for (let j = 0; j < yLength; j++) {
+                            if (!this.array[i][j].opened) {
+                                bArea.children[j].children[i].classList.add("bomb");
+                            }
+                        }
+                    }
+                    win();
+                }
+                x = parseInt(x);
+                y = parseInt(y);
+                const AROUND = [
+                    [x - 1, y - 1],
+                    [x, y - 1],
+                    [x + 1, y - 1],
+                    [x - 1, y],
+                    [x + 1, y],
+                    [x - 1, y + 1],
+                    [x, y + 1],
+                    [x + 1, y + 1],
+                ];
+                let count = 0;
+                AROUND.forEach((item) => {
+                    if (between(item[0], xLength) && between(item[1], yLength)) {
+                        if (((this.array[item[0]] || {})[item[1]] || {}).isMine) {
+                            count++;
+                        }
+                    }
+                });
+                if (count == 0) {
+                    count = ""; //不显示0
+                    AROUND.forEach((item) => {
+                        if (between(item[0], xLength) && between(item[1], yLength)) {
+                            if (!(this.array[item[0]][item[1]].opened)) {
+                                return this.openBlock(item[0], item[1]);
+                            }
+                        }
+                    });
+                } else {
+                    bArea.children[y].children[x].innerHTML = count;
+                }
+            }
+        } else {
+            lose(x, y);
+        }
     }
 }
 
+let game = new Block();
+
+//Result
 function win() {
-    // clearInterval(timer);
-    console.log(win);
+    clearInterval(timer);
+    isOver = true;
+    window.setTimeout(() => confirmNext("win"), 500);
 }
 
 function lose(x, y) {
-    // clearInterval(timer);
-    console.log(lose);
+    clearInterval(timer);
+    isOver = true;
     bArea.children[y].children[x].classList.add("bomb0");
-    for (let i = 0; i < sLength; i++) {
-        for (let j = 0; j < sLength; j++) {
-            if (a1.array[i][j].ismine) {
+    for (let i = 0; i < xLength; i++) {
+        for (let j = 0; j < yLength; j++) {
+            if (game.array[i][j].isMine) {
                 bArea.children[j].children[i].classList.add("bomb");
             }
         }
     }
+    window.setTimeout(() => confirmNext("lose"), 500);
+}
+
+function confirmNext(result) {
+    switch (result) {
+        case "win":
+            if (confirm("胜利！要进行下盘游戏吗？")) {
+                Reset();
+                game.setMinefield(xLength, yLength);
+            }
+            break;
+        case "lose":
+            if (confirm("糟糕踩到雷啦！要不要重开一盘？")) {
+                Reset();
+                game.setMinefield(xLength, yLength);
+            }
+            break;
+    }
+}
+
+//NewGame
+function newGame() {
+    if (!bArea.firstChild) {
+        game.setMinefield(xLength, yLength);
+    } else if (confirm("是否要重置游戏？")) {
+        Reset();
+        game.setMinefield(xLength, yLength);
+    }
+}
+
+function Reset() {
+    clearInterval(timer);
+    isOver = false;
+    step = 0;
+    openedBlock = 0;
+    time = 0;
+    flaggedMine = 0;
+    document.getElementById("timer").innerHTML = time;
+    document.getElementById("mine").innerHTML = mineNum;
+    document.getElementById("step").innerHTML = step;
+    game.removeMinefield();
+    game.array = [];
+}
+
+//Level
+function selectLevel(level) {
+    switch (level) {
+        case "easy":
+            mineNum = 10;
+            xLength = 9;
+            yLength = 9;
+            break;
+        case "normal":
+            mineNum = 40;
+            xLength = 16;
+            yLength = 16;
+            break;
+        case "hard":
+            mineNum = 99;
+            xLength = 30;
+            yLength = 16;
+            break;
+    }
+    newGame();
+    document.getElementById("mine").innerHTML = mineNum;
+}
+
+//Tools
+function Timer() {
+    timer = setInterval(() => {           //issue：多次点击好像会创建很多timer，加速计时
+        time++;
+        document.getElementById("timer").innerHTML = time;
+    }, 1000);
 }
 
 function between(x, max) {
     return x >= 0 && x <= (max - 1);
 }
 
-function open(x, y) {
-    if (a1.array[0] == undefined) {
-        a1.setBlock(x, y);
-    }
-    openBlock(x, y);
-    step++;
-    document.getElementById("step").innerHTML = step;
-}
 
-function openBlock(x, y) {
-    if (!a1.array[x][y].ismine) {
-        if (!a1.array[x][y].flagged) {
-            a1.array[x][y].opened = true;
-            bArea.children[y].children[x].setAttribute("class", "opened");
-            openedBlock++;
-            notMine = sLength * sLength - mineNum;
-            if (openedBlock == notMine) {
-                win();
-            }
-            x = parseInt(x);
-            y = parseInt(y);
-            const around = [
-                [x - 1, y - 1],
-                [x, y - 1],
-                [x + 1, y - 1],
-                [x - 1, y],
-                [x + 1, y],
-                [x - 1, y + 1],
-                [x, y + 1],
-                [x + 1, y + 1],
-            ];
-            let count = 0;
-            around.forEach(function(item) {
-                if (between(item[0], sLength) && between(item[1], sLength)) {
-                    if (((a1.array[item[0]] || {})[item[1]] || {}).ismine) {
-                        count++;
-                    }
-                }
-            });
-            if (count == 0) {
-                count = ""; //不显示0
-                around.forEach(function(item) {
-                    if (between(item[0], sLength) && between(item[1], sLength)) {
-                        if (!(a1.array[item[0]][item[1]].opened)) {
-                            return openBlock(item[0], item[1]);
-                        }
-                    }
-                });
-            } else {
-                bArea.children[y].children[x].innerHTML = count;
-            }
-        }
-    } else {
-        lose(x, y);
-    }
-}
 
-function flag(x, y) {
-    if (!a1.array[x][y].opened) {
-        if (!a1.array[x][y].flagged) {
-            a1.array[x][y].flagged = true;
-            bArea.children[y].children[x].classList.add("flagged");
-        } else {
-            a1.array[x][y].flagged = false;
-            bArea.children[y].children[x].classList.remove("flagged");
-        }
-    }
-}
+//------------------------------------------------------------------
+//
+// function doubleClick(x, y) {
+//     if (game.array[x][y].opened) {
 
-function doubleClick(x, y) {
-    if (a1.array[x][y].opened) {
-
-        const around = [
-            [x - 1, y - 1],
-            [x, y - 1],
-            [x + 1, y - 1],
-            [x - 1, y],
-            [x + 1, y],
-            [x - 1, y + 1],
-            [x, y + 1],
-            [x + 1, y + 1],
-        ];
-        let mCount, fCount;
-        around.forEach(function(item) {
-            if ((a1.array[item[0]] || {})([item[1]] || {}).ismine) {
-                mCount++;
-            }
-            if ((a1.array[item[0]] || {})([item[1]] || {}).flagged) {
-                fCount++;
-            }
-        });
-        if (mCount == fCount) {
-            around.forEach(function(item) {
-                if ((a1.array[item[0]] || {})([item[1]] || {}).ismine) {
-                    if (!((a1.array[item[0]] || {})([item[1]] || {}).flagged)) {
-                        lose();
-                    }
-                } else {
-                    openBlock((a1.array[item[0]] || {}), (a1.array[item[1]] || {}));
-                }
-            });
-        }
-    }
-}
-
-document.getElementById("newBtn").addEventListener("click", () => newGame(sLength, sLength, mineNum));
-document.getElementById("mine").innerHTML = mineNum;
-document.getElementById("step").innerHTML = step;
-document.getElementById("timer").innerHTML = time;
+//         const around = [
+//             [x - 1, y - 1],
+//             [x, y - 1],
+//             [x + 1, y - 1],
+//             [x - 1, y],
+//             [x + 1, y],
+//             [x - 1, y + 1],
+//             [x, y + 1],
+//             [x + 1, y + 1],
+//         ];
+//         let mCount, fCount;
+//         around.forEach(function(item) {
+//             if ((game.array[item[0]] || {})([item[1]] || {}).isMine) {
+//                 mCount++;
+//             }
+//             if ((game.array[item[0]] || {})([item[1]] || {}).flagged) {
+//                 fCount++;
+//             }
+//         });
+//         if (mCount == fCount) {
+//             around.forEach(function(item) {
+//                 if ((game.array[item[0]] || {})([item[1]] || {}).isMine) {
+//                     if (!((game.array[item[0]] || {})([item[1]] || {}).flagged)) {
+//                         lose();
+//                     }
+//                 } else {
+//                     openBlock((game.array[item[0]] || {}), (game.array[item[1]] || {}));
+//                 }
+//             });
+//         }
+//     }
+// }
